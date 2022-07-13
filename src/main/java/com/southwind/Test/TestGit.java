d57f47bcb2f5a6e95254130983a09f4d2ee1d939
lzk <729765221@qq.com>
2022-07-13 13:33:15 +0800

    hot-fix commit

--- src/main/java/com/southwind/Test/TestGit.java
+++ src/main/java/com/southwind/Test/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello hot-fix");
     }
 }
