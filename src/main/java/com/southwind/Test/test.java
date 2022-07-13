dea9c6869710226b0e81cfac3fbcffd4fa70df0d
lzk <729765221@qq.com>
2022-07-13 13:26:23 +0800

    hot-fix second commit

---
+++ src/main/java/com/southwind/Test/test.java
@@ -0,0 +1,11 @@
+package com.southwind.Test;
+
+/**
+ * @author lzk
+ * @create 2022-07-13 13:25
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello,git,hot-fix");
+    }
+}
